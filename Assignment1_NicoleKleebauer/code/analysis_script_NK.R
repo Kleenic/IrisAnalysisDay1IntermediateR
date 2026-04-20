381648879500238deb5f32701202cf52a9ee30f4
Nicole Kleebauer <nici.kleebauer@gmail.com>
2026-04-20 17:11:25 +0200

    First test to push some lines of script, just to see if everything is in place.

---
+++ Assignment1_NicoleKleebauer/code/analysis_script_NK.R
@@ -0,0 +1,10 @@
+# Assignment 1 Day 1
+
+# Starting by saving the R script in the correct subfolder and 
+# installing and loading all necessary libraries.
+
+dir.create("/Assignment1_NicoleKleebauer/")
+# This gave an error "Read-only file system"?
+# Created folder manually
+
+library(tidyverse)
